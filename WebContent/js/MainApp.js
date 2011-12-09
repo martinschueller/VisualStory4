d39788ee6318bb18b18ce9ad3cebf8c40c4c4974
martin schueller <martin_schuller@martins-mbp.local>
2011-12-09 11:11:57 -0800

    first commit

---
+++ WebContent/js/MainApp.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	
+	
+	
+});
+
+
+
+
+//Randomizes divs
+(function($) {
+
+	$.fn.randomize = function(childElem) {
+	  return this.each(function() {
+	      var $this = $(this);
+	      var elems = $this.children(childElem);
+
+	      elems.sort(function() { return (Math.round(Math.random())-0.5); });  
+
+	      $this.remove(childElem);  
+
+	      for(var i=0; i < elems.length; i++)
+	        $this.append(elems[i]);      
+
+	  });    
+	};
+});
